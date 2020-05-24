0ccba1523e00ec0c1b78a99543904bda874dc8b0
hansonlam1 <hansonlam1@gmail.com>
2020-05-23 17:39:02 -0700

    added ideas py

---
+++ tradeideas.py
@@ -0,0 +1,11 @@
+import datetime as dt
+import pandas as pd
+
+# test some trade ideas
+
+def trade8020(df):
+    # if the previous day was a 8020 setup long (reverse for shorts)
+    # did today retest yesterdays low?
+    # and did today go back about the yesterdays low?
+
+    return df
